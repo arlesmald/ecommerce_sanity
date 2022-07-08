ee9430bee10c79daa89ae177ae9e949ccf70ec81
arlesmald <40522215+arlesmald@users.noreply.github.com>
2022-07-08 14:25:48 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components'
-import { StateContext } from '../Context/StateContext'
+import { StateContext } from '../context/StateContext'
 
 function MyApp({ Component, pageProps }) {
   return (
