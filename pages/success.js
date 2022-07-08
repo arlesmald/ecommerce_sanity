d40eb01cde60fcd67b0fa53768d5c871c8bb3f8a
arlesmald <40522215+arlesmald@users.noreply.github.com>
2022-07-08 14:21:05 -0600

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
 
-import { useStateContext } from '../Context/StateContext';
+import { useStateContext } from '../context/StateContext';
 import { runFireworks } from '../lib/utils';
 
 const success = () => {
